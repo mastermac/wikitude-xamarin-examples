bc9f371d5dd5bb17011d9b4b28eeff889946e607
Shubham Gupta <shubham_gupta5@dell.com>
2019-06-10 19:56:32 +0530

    Updated Codebase

--- XamarinExampleApp/Droid/Assets/samples/11_Video_3_SnappingVideo/js/snappingvideo.js
+++ XamarinExampleApp/Droid/Assets/samples/11_Video_3_SnappingVideo/js/snappingvideo.js
@@ -1,4 +1,29 @@
+var defaultScaleValue = 0.5;
+var video;
+var previousRotationValue = [];
+var previousScaleValue = [];
+
+var oneFingerGestureAllowed = false;
+
+AR.context.on2FingerGestureStarted = function () {
+    oneFingerGestureAllowed = false;
+};
+
 var World = {
+    paths: [
+        "assets/christmas_hat.png",
+        "assets/police_hat.png",
+        "assets/inspire.png",
+        "assets/patent.png",
+        "assets/hackathon.png",
+        "assets/hackathon.png",
+        "assets/csr.png"
+    ],
+    imageTrackables: [],
+    overlays: [],
+    appearingAnimations: [],
+    targetAcquired: false,
+
     loaded: false,
 
 
@@ -142,7 +167,165 @@ var World = {
             },
             onError: World.onError
         });
+
+        World.initPositionValues();
+        for (var i = 0; i < this.paths.length; i++) {
+            World.createOverlayWithIndex(i);
+        }
+
+        World.setupAppearingAnimations();
+
     },
+
+    /* Sets up the initial positions of every draggable. */
+    initPositionValues: function () {
+        var numberOfOverlays = this.paths.length;
+
+        previousRotationValue = World.fillArray(0.0, numberOfOverlays);
+        previousScaleValue = World.fillArray(defaultScaleValue, numberOfOverlays);
+    },
+
+    /* Returns an array with "len" occurrences of "value" */
+    fillArray: function (value, len) {
+        var arr = [];
+        for (var i = 0; i < len; i++) {
+            arr.push(value);
+        }
+        return arr;
+    },
+    createOverlayWithIndex: function (index) {
+        var imageResource = new AR.ImageResource(this.paths[index], {
+            onError: World.onError
+        });
+
+        var overlay = new AR.ImageDrawable(imageResource, 1, {
+            scale: {
+                x: defaultScaleValue,
+                y: defaultScaleValue
+            },
+            onDragBegan: function ( /*x, y*/) {
+                oneFingerGestureAllowed = true;
+
+                return true;
+            },
+            onDragChanged: function (x, y, intersectionX, intersectionY) {
+                if (oneFingerGestureAllowed) {
+                    this.translate = {
+                        x: intersectionX,
+                        y: intersectionY
+                    };
+                }
+
+                return true;
+            },
+            onDragEnded: function ( /*x, y*/) {
+                return true;
+            },
+            onRotationBegan: function ( /*angleInDegrees*/) {
+                return true;
+            },
+            onRotationChanged: function (angleInDegrees) {
+                this.rotate.z = previousRotationValue[index] + angleInDegrees;
+
+                return true;
+            },
+            onRotationEnded: function ( /*angleInDegrees*/) {
+                previousRotationValue[index] = this.rotate.z;
+
+                return true;
+            },
+            onScaleBegan: function ( /*scale*/) {
+                return true;
+            },
+            onScaleChanged: function (scale) {
+                var scaleValue = previousScaleValue[index] * scale;
+                this.scale = {
+                    x: scaleValue,
+                    y: scaleValue
+                };
+
+                return true;
+            },
+            onScaleEnded: function ( /*scale*/) {
+                previousScaleValue[index] = this.scale.x;
+
+                return true;
+            }
+        });
+
+        this.overlays.push(overlay);
+
+        var imageTrackable = new AR.ImageTrackable(this.tracker, "*", {
+            drawables: {
+                cam: [overlay]
+            },
+            onImageRecognized: World.imageRecognized,
+            onImageLost: World.imageLost,
+            onError: World.onError
+        });
+        imageTrackable.enabled = false;
+
+        this.imageTrackables.push(imageTrackable);
+    },
+    setupAppearingAnimations: function () {
+        for (var i = 0; i < this.overlays.length; i++) {
+            this.appearingAnimations[i] = World.createAppearingAnimation(this.overlays[i], defaultScaleValue);
+        }
+    },
+
+    createAppearingAnimation: function createAppearingAnimationFn(overlay, scale) {
+        /*
+            The animation scales up the overlay once the target is inside the field of vision. Creating an animation on
+            a single property of an object is done using an AR.PropertyAnimation. Since the overlays only need to be
+            scaled up on two axis, two animations are needed. These animations are grouped together utilizing an
+            AR.AnimationGroup that allows them to play them in parallel.
+
+            Each AR.PropertyAnimation targets one of the two axis and scales the model from 0 to the value passed in
+            the scale variable. An easing curve is used to create a more dynamic effect of the animation.
+        */
+        var sx = new AR.PropertyAnimation(overlay, "scale.x", 0, scale, 1500, {
+            type: AR.CONST.EASING_CURVE_TYPE.EASE_OUT_ELASTIC
+        });
+        var sy = new AR.PropertyAnimation(overlay, "scale.y", 0, scale, 1500, {
+            type: AR.CONST.EASING_CURVE_TYPE.EASE_OUT_ELASTIC
+        });
+
+        return new AR.AnimationGroup(AR.CONST.ANIMATION_GROUP_TYPE.PARALLEL, [sx, sy]);
+    },
+
+    /* Makes an overlay visible by enabling its AR.ImageTrackable. */
+    showOverlay: function (index) {
+        //if (World.targetAcquired) {
+        if (!this.imageTrackables[index].enabled) {
+            this.imageTrackables[index].enabled = true;
+
+            this.appearingAnimations[index].start();
+        }
+        //}
+    },
+    clearOverlays: function () {
+
+        //if (World.targetAcquired) {
+        for (var i = 0; i < this.overlays.length; i++) {
+            World.resetOverlayWithIndex(i);
+        }
+        for (var u = 0; u < this.imageTrackables.length; u++) {
+            this.imageTrackables[u].enabled = false;
+        }
+        World.initPositionValues();
+        //}
+    },
+
+    /* Resets the parameters of an overlay to its initial values. */
+    resetOverlayWithIndex: function (index) {
+        var overlay = this.overlays[index];
+        overlay.translate.x = 0.0;
+        overlay.translate.y = 0.0;
+        overlay.rotate.z = 0.0;
+        overlay.scale.x = defaultScaleValue;
+        overlay.scale.y = defaultScaleValue;
+    },
+
 
     imageRecognized: function () {
         //video.resume();
@@ -178,7 +361,7 @@ var World = {
             case "9114135d-da83-4c3f-9618-a42acf8a3fe0": LocalBanda = "  Sita T - Director";
                 FunFact = "Fun Fact: A Shopaholic! ";
                 break;
-            default: LocalBanda = "unknown";
+            default: LocalBanda = " User Not Registered...";
         }
         if (World.banda != LocalBanda) {
             World.banda = LocalBanda;
